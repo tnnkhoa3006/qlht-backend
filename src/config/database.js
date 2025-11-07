38284f8f071e462e8802c2ad591c7ac85218fcc1
Khoa Tran <khoat0360@gmail.com>
2025-11-07 21:55:12 +0700

    add: add connection database for supabase

--- src/config/database.js
+++ src/config/database.js
@@ -6,12 +6,10 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
-  ssl: false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('connect', () => {
